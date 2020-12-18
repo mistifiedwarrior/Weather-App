492bb8f6fd89ed254d19c541145b47cc913e9fad
Shivam Rajput <58025059+shiviraj@users.noreply.github.com>
2020-12-18 19:22:27 +0530

    add setup.sh and start script

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const request = require('request');
 
-const url =
-  'http://api.weatherstack.com/current?access_key=fb90918118afe67d007110a860a7892a&query=ballia';
+const url = `http://api.weatherstack.com/current?access_key=${process.env.ACCESS_TOKEN}&query=ballia`;
 
-request({url: url}, (error, response) => {
+request({ url }, (error, response) => {
   const data = JSON.parse(response.body);
   console.log(data.current);
 });
