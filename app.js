0c947e990c0c23178afccb2951ced44f2f3f28b9
mistifiedwarrior <bagiballia@users.noreply.github.com>
2020-12-31 12:08:44 +0530

    added latitude & longitude

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const request = require('request');
 
-const url = `http://api.weatherstack.com/current?access_key=${process.env.ACCESS_TOKEN}&query=ballia`;
+const address = 'Ballia';
+const url = `http://api.weatherstack.com/current?access_key=${process.env.ACCESS_TOKEN}&query=${address}`;
 
-request({ url }, (error, response) => {
+request({url}, (error, response) => {
   if (error) {
     console.log('unable to connect to weatherStack server ');
-    return
+    return;
   }
   const data = JSON.parse(response.body);
   if (data.error) {
-    console.log(data.error.info)
+    console.log(data.error.info);
     return;
   }
   const currentData = `${data.current.weather_descriptions} It is currently ${data.current.temperature} degrees out, and there is ${data.current.precip} % chance of raining `;
   console.log(currentData);
+  console.log(data.location.lat, data.location.lon);
 });
