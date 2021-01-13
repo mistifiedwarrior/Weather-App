ab6483ffd2cd8b5de322d476bd7c82572481d2b2
mistifiedwarrior <64316262+mistifiedwarrior@users.noreply.github.com>
2021-01-13 16:24:38 +0530

    input taken from command line

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 const request = require('request');
 
-const address = 'Ballia';
+const address = process.argv[2];
 const token = process.env.ACCESS_TOKEN;
 const url = `http://api.weatherstack.com/current?access_key=${token}&query=${address}`;
 
 const foreCastData = function (data) {
-  console.log(data.location.lat, '\n', data.location.lon);
   console.log(data.location.name);
+  console.log(data.location.lat, '\n', data.location.lon);
 };
 
 request({url}, (error, response) => {
   if (error) {
-    console.log('unable to connect to weatherStack server ');
-    return;
+    return console.log('unable to connect to weatherStack server ');
   }
   const data = JSON.parse(response.body);
   if (data.error) {
-    console.log(data.error.info);
-    return;
+    return console.log(data.error.info);
   }
   const currentData = `${data.current.weather_descriptions} It is currently ${data.current.temperature} degrees out, and there is ${data.current.precip} % chance of raining `;
   console.log(currentData);
