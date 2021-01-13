f4dfdc82b68f73f5be597ab49760f4b3dfb62873
mistifiedwarrior <64316262+mistifiedwarrior@users.noreply.github.com>
2021-01-13 15:19:40 +0530

    modified the way of accessing latitude and longitude

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const address = 'Ballia';
 const token = process.env.ACCESS_TOKEN;
 const url = `http://api.weatherstack.com/current?access_key=${token}&query=${address}`;
 
+const foreCastData = function (data) {
+  console.log(data.location.lat, '\n', data.location.lon);
+  console.log(data.location.name);
+};
+
 request({url}, (error, response) => {
   if (error) {
     console.log('unable to connect to weatherStack server ');
@@ -16,5 +21,5 @@ request({url}, (error, response) => {
   }
   const currentData = `${data.current.weather_descriptions} It is currently ${data.current.temperature} degrees out, and there is ${data.current.precip} % chance of raining `;
   console.log(currentData);
-  console.log(data.location.lat, data.location.lon);
+  foreCastData(data);
 });
