5f0e9f04b0720bc07203fd9183b180b29fd4e2a0
mistifiedwarrior <64316262+mistifiedwarrior@users.noreply.github.com>
2021-01-14 20:37:41 +0530

    removed access key

--- playground/4-https.js
+++ playground/4-https.js
@@ -1,6 +1,7 @@
 const http = require('http');
-const url =
-  'http://api.weatherstack.com/current?access_key=fb90918118afe67d007110a860a7892a&query=Ballia';
+const address = process.argv[2];
+const token = process.env.ACCESS_TOKEN;
+const url = `http://api.weatherstack.com/current?access_key=${token}&query=${address}`;
 const request = http.request(url, (response) => {
   let data = '';
   response.on('data', (chunk) => {
