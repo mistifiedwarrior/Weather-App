36d0ce316c27008d1ec6a12d6aad4a6d22b9a836
Shivam Rajput <58025059+shiviraj@users.noreply.github.com>
2021-01-26 10:56:38 +0530

    Update runTest.sh

--- runTest.sh
+++ runTest.sh
@@ -2,9 +2,12 @@ cat countVowels.js > tempCountVowels.js
 cat countConsonants.js > tempCountConsonants.js
 cat isPalindrome.js > tempIsPalindrome.js
 
-echo "module.exports = { countVowels }" >> tempCountVowels.js
-echo "module.exports = { countConsonants }" >> tempCountConsonants.js
-echo "module.exports = { isPalindrome }" >> tempIsPalindrome.js
+echo "
+module.exports = { countVowels }" >> tempCountVowels.js
+echo "
+module.exports = { countConsonants }" >> tempCountConsonants.js
+echo "
+module.exports = { isPalindrome }" >> tempIsPalindrome.js
 
 curl https://raw.githubusercontent.com/mistifiedwarrior/Weather-App/shivi/runAllTest.js > runAllTest.js
 
