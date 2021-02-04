863e127668b3b00debffe9a0b746657b8f357561
mistifiedwarrior <64316262+mistifiedwarrior@users.noreply.github.com>
2021-02-04 12:46:46 +0530

    learned webserver

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 console.log(__dirname);
 console.log(path.join(__dirname, '../public'));
@@ -8,23 +9,58 @@ const app = express();
 
 //defining path for express config:
 const publicDirectory = path.join(__dirname, '../public');
-const viewPath = path.join(__dirname, '../views');
+const viewPath = path.join(__dirname, '../templates/views');
+const partialPath = path.join(__dirname, '../templates/partials');
 
 //set up handlebars engines and views location:
 app.set('view engine', 'hbs');
 app.set('views', viewPath);
+hbs.registerPartials(partialPath);
 
 //Setup static directory to serve:
 app.use(express.static(publicDirectory));
 
 app.get('/view', (req, res) => {
-  res.render('index');
+  res.render('index', {
+    title: 'Weather',
+    name: 'Aditi',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'about me',
+    name: 'Aditi',
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'help page',
+    name: 'Aditi',
+  });
 });
 
 app.get('/weather', (req, res) => {
   res.send({
     forecast: 'It is snowing',
     location: 'Ballia',
+  });
+});
+
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Aditi',
+    errorMessage: 'help articles not found',
+  });
+});
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Aditi',
+    errorMessage: 'Page not found',
   });
 });
 app.listen(3000, () => console.log('servers is up on port 3000'));
