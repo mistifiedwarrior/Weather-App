e7d6082ddfb39ab65a2794fe92662e36508cf45a
mistifiedwarrior <64316262+mistifiedwarrior@users.noreply.github.com>
2021-02-04 15:33:41 +0530

    query string

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -42,9 +42,28 @@ app.get('/help', (req, res) => {
 });
 
 app.get('/weather', (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: 'you must provide an address',
+    });
+  }
   res.send({
-    forecast: 'It is snowing',
-    location: 'Ballia',
+    address: req.query.address,
+    forecast: 'Its raining',
+    temperature: '55 degrees',
+  });
+});
+
+app.get('/product', (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: 'you must provide a search term',
+    });
+  }
+
+  console.log(req.query.search);
+  res.send({
+    products: [],
   });
 });
 
