d6c9491db9f4b02f506ed3d5451537c451e784a3
mistifiedwarrior <64316262+mistifiedwarrior@users.noreply.github.com>
2021-02-21 11:53:33 +0530

    added icon

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,7 @@
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const $details = document.querySelector('.details');
+const $error = document.querySelector('#error');
 const $location = document.querySelector('#location');
 const $description = document.querySelector('#description');
 const $temp = document.querySelector('#temp');
@@ -10,6 +12,7 @@ const $windDirection = document.querySelector('#wind_dir');
 const $visibility = document.querySelector('#visibility');
 const $lat = document.querySelector('#lat');
 const $lon = document.querySelector('#lon');
+const $icon = document.querySelector('#img');
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -21,19 +24,24 @@ weatherForm.addEventListener('submit', (e) => {
   fetch(`/weather?address=${searchLocation}`).then((response) => {
     response.json().then((data) => {
       if (data.error) {
-        $location.innerText = data.error;
+        $error.innerText = data.error;
+        $details.style.display = 'none';
+        $error.style.display = 'block';
       } else {
         const {location, current} = data;
         $location.innerText = `Location: ${location.name} ${location.region}, ${location.country}`;
         $description.innerText = `Description: ${current.weather_descriptions}`;
         $temp.innerText = `Temp: ${current.temperature} °C`;
         $isDay.innerText = `${current.is_day === 'yes' ? 'Day' : 'Night'}`;
+        $icon.src = current.weather_icons[0];
         $pressure.innerText = `Pressure: ${current.pressure} MB - millibar`;
         $humidity.innerText = `Humidity: ${current.humidity}%`;
         $windDirection.innerText = `Wind: ${current.wind_dir} ${current.wind_speed}km/hr`;
         $visibility.innerText = `Visibility: ${current.visibility} KM`;
         $lat.innerText = `Latitudes: ${location.lat}`;
         $lon.innerText = `Longitudes: ${location.lon}`;
+        $details.style.display = 'block';
+        $error.style.display = 'none';
       }
     });
   });
--- src/weatherDetails.js
+++ src/weatherDetails.js
@@ -5,18 +5,18 @@ const getWeatherDetails = (req, res) => {
   const token = process.env.ACCESS_TOKEN;
   const url = `http://api.weatherstack.com/current?access_key=${token}&query=${address}`;
   if (!address) {
-    res.send({
+    return res.send({
       error: 'Please provide the proper address',
     });
   }
 
   request({url}, (error, response) => {
     if (error) {
-      return console.log('unable to connect to weatherStack server');
+      return res.send({error: 'unable to connect to weatherStack server'});
     }
     const data = JSON.parse(response.body);
     if (data.error) {
-      return console.log(data.error.info);
+      return res.send({error: data.error.info});
     }
 
     res.send(data);
